3adeb3b986b07b21c53106fe39f8056b9d8cb460
jcmunav63 <jcmunav63@gmail.com>
2023-04-18 09:24:08 -0600

    Edit css and js files to let the menu show

--- js/mobile.js
+++ js/mobile.js
@@ -1,8 +1,8 @@
 const menuToggle = document.querySelector('.menu');
-const menu-list = document.querySelector('.menu-list');
+const modal = document.querySelector('.overlay');
 const closeButton = document.querySelector('.cancel1');
 
 menuToggle.addEventListener('click', () => {
-  toolbar-2.add('show');
+  modal.add('show');
   menu-list.classList.add('show');
 });
