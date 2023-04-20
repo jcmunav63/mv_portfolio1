be42ed4cadcda73c71929f0f4a31181e51ac9c74
jcmunav63 <jcmunav63@gmail.com>
2023-04-19 20:31:53 -0600

    Edit css, html & js files to add email input validation.

--- js/mobile.js
+++ js/mobile.js
@@ -15,3 +15,24 @@ modal.querySelectorAll('a').forEach((link) => {
     modal.classList.remove('show');
   });
 });
+
+const form = document.getElementById("form1");
+const email = form["email"];
+
+let email2 = email.value;
+const pattern = new RegExp(/^[0-9a-z\_]+@[0-9a-z\.]+\.[a-z]{2,4}$/);
+// /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,63})$
+// /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  alert("The submit button is working!");
+});
+
+if (pattern.test(email2) = false) {
+  //show the error message
+  alert('Email is invalid! Use lowercase letters.');
+} else {
+  //All good! Letters are lowercase. Send the form.
+  
+}
