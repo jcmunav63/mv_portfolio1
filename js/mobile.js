f60bfa6b1cfc7cb60de336e736c364f6bee2c418
baezluise <107555064+baezluise@users.noreply.github.com>
2023-04-20 12:42:18 -0500

    Interaction added
    -prompt invalid gmail when is Uppercase

--- js/mobile.js
+++ js/mobile.js
@@ -16,35 +16,30 @@ modal.querySelectorAll('a').forEach((link) => {
   });
 });
 
-
-
-const form = document.getElementById("form1");
-const email = form["email"];
+const form = document.getElementById('form1');
+const email = form['email'];
 
 function emailValidation(mail) {
   const pattern = new RegExp(/^[0-9a-z\_]+@[0-9a-z\.]+\.[a-z]{2,4}$/);
-  if (pattern.test(mail) == false) {
+  if (pattern.test(mail) === false) {
     return false;
   } else {
     return true;
   }
 }
-// Enmail validation alternatives REGEXP
-// /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,63})$
-// /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
 
-form.addEventListener("submit", (event) => {
+form.addEventListener('submit', (event) => {
   event.preventDefault();
-  alert("The submit button is working!");
 
   let email2 = email.value;
-  alert("Email: " + email2);
   let isValid = emailValidation(email2);
+  const msg = document.querySelector('.message1');
   if (!isValid) {
-    alert('Email is invalid! Use lowercase letters.');
+    msg.innerHTML = `<img class="" href="/images/cancel1.png" />
+    <p class="message1-p">Error: Email needs to be in lowercase letters!</p>
+                   `;
   } else {
     form.submit();
     form.reset();
   }
-
 });
