bfae8fed621347d08960cd20a74c1bedf3b704f3
jcmunav63 <jcmunav63@gmail.com>
2023-04-21 09:47:56 -0600

    Add functions for storing and updating data

--- js/scripts.js
+++ js/scripts.js
@@ -2,59 +2,79 @@
 
 // FIRST, TAKE THE INFO FROM 3 INPUTS AN STORE IN LOCALSTORAGE (JSON).
 
+window.addEventListener('load', myFunction() {
+  myFunction();
+  // console.log('La página ha terminado de cargarse!!');
+});
+
+myFunction() {
+  const myContactData = { name: 'Francisco Garcia', email: 'fgarcia@email.com', message: 'This is my message' }
+  
+  localStorage.setItem('contactForm', JSON.stringify(myContactData));
+}
+
+
 // THEN, IF THERE IS ANY DATA IN LOCALSTORAGE, WHEN THE PAGE LOADS,
 // THE INPUT FIELDS ARE PREFILLED WITH THE DATA.
 
 // THIRD, WHEN THE USER CHANGES ANY INPUT FIELD, THE DATA IS SAVED IN
 // LOCALSTORAGE
+// Get the form elements
+  const nameInput = document.querySelector('#names');
+  const emailInput = document.querySelector('#email');
+  const messageInput = document.querySelector('#textarea');
+
+  let string1 = "{ name: " + nameInput + ", email: " + emailInput + ", message: " + messageInput + "}";
+
+  localStorage.setItem('contactForm', JSON.stringify(string1);
 
 //  E X A M P L E S
-if (!localStorage.getItem("bgcolor")) {
-    populateStorage();
-  } else {
-    setStyles();
-  }
+// if (!localStorage.getItem("bgcolor")) {
+//     populateStorage();
+//   } else {
+//     setStyles();
+//   }
 
-  function setStyles() {
-    const currentColor = localStorage.getItem("bgcolor");
-    const currentFont = localStorage.getItem("font");
-    const currentImage = localStorage.getItem("image");
+//   function setStyles() {
+//     const currentColor = localStorage.getItem("bgcolor");
+//     const currentFont = localStorage.getItem("font");
+//     const currentImage = localStorage.getItem("image");
   
-    document.getElementById("bgcolor").value = currentColor;
-    document.getElementById("font").value = currentFont;
-    document.getElementById("image").value = currentImage;
+//     document.getElementById("bgcolor").value = currentColor;
+//     document.getElementById("font").value = currentFont;
+//     document.getElementById("image").value = currentImage;
   
-    htmlElem.style.backgroundColor = `#${currentColor}`;
-    pElem.style.fontFamily = currentFont;
-    imgElem.setAttribute("src", currentImage);
-  }
+//     htmlElem.style.backgroundColor = `#${currentColor}`;
+//     pElem.style.fontFamily = currentFont;
+//     imgElem.setAttribute("src", currentImage);
+//   }
 
-  function populateStorage() {
-    localStorage.setItem("bgcolor", document.getElementById("bgcolor").value);
-    localStorage.setItem("font", document.getElementById("font").value);
-    localStorage.setItem("image", document.getElementById("image").value);
+//   function populateStorage() {
+//     localStorage.setItem("bgcolor", document.getElementById("bgcolor").value);
+//     localStorage.setItem("font", document.getElementById("font").value);
+//     localStorage.setItem("image", document.getElementById("image").value);
   
-    setStyles();
-  }
+//     setStyles();
+//   }
 
-bgcolorForm.onchange = populateStorage;
-fontForm.onchange = populateStorage;
-imageForm.onchange = populateStorage;
+// bgcolorForm.onchange = populateStorage;
+// fontForm.onchange = populateStorage;
+// imageForm.onchange = populateStorage;
 
 
-window.addEventListener("storage", (e) => {
-    document.querySelector(".my-key").textContent = e.key;
-    document.querySelector(".my-old").textContent = e.oldValue;
-    document.querySelector(".my-new").textContent = e.newValue;
-    document.querySelector(".my-url").textContent = e.url;
-    document.querySelector(".my-storage").textContent = JSON.stringify(
-      e.storageArea
-    );
-  });
+// window.addEventListener("storage", (e) => {
+//     document.querySelector(".my-key").textContent = e.key;
+//     document.querySelector(".my-old").textContent = e.oldValue;
+//     document.querySelector(".my-new").textContent = e.newValue;
+//     document.querySelector(".my-url").textContent = e.url;
+//     document.querySelector(".my-storage").textContent = JSON.stringify(
+//       e.storageArea
+//     );
+//   });
 
-  Storage.removeItem(); 
-  // takes a single argument — the key of the data item you want to
-  // remove — and removes it from the storage object for that domain.
-  Storage.clear(); 
+//   Storage.removeItem(); 
+//   // takes a single argument — the key of the data item you want to
+//   // remove — and removes it from the storage object for that domain.
+//   Storage.clear(); 
   // takes no arguments, and empties the entire storage object for
   // that domain.
