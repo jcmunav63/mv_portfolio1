b3839766a7517d8b477161a37eb347765bc126c7
jcmunav63 <jcmunav63@gmail.com>
2023-04-20 18:57:26 -0600

    Edit CSS & JS files to correct linters errors.

--- js/mobile.js
+++ js/mobile.js
@@ -19,33 +19,32 @@ modal.querySelectorAll('a').forEach((link) => {
 
 // EMAIL VALIDATION SCRIPT
 const form = document.getElementById('form1');
-const email = form['email'];
+const { email } = form;
 
 function emailValidation(mail) {
-  const pattern = new RegExp(/^[0-9a-z\_]+@[0-9a-z\.]+\.[a-z]{2,4}$/);
+  const pattern = new RegExp(/^[0-9a-z_]+@[0-9a-z.]+\.[a-z]{2,4}$/);
   if (pattern.test(mail) === false) {
     return false;
-  } else {
-    return true;
   }
+  return true;
 }
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  let isValid = emailValidation(email.value);
-  var modal2 = document.getElementById("myModal");
-  var span = document.getElementsByClassName("close")[0];
+  const isValid = emailValidation(email.value);
+  const modal2 = document.getElementById('myModal');
+  const span = document.getElementsByClassName('close')[0];
 
   if (!isValid) {
-    modal2.style.display = "block";
-    span.onclick = function() {
-      modal2.style.display = "none";
-    }
-    window.onclick = function(event) {
-      if (event.target == modal2) {
-        modal2.style.display = "none";
+    modal2.style.display = 'block';
+    span.onclick = function () {
+      modal2.style.display = 'none';
+    };
+    window.onclick = function (event) {
+      if (event.target === modal2) {
+        modal2.style.display = 'none';
       }
-    }
+    };
   } else {
     form.submit();
     form.reset();
